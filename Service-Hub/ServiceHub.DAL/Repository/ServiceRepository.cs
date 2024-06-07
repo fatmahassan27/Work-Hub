5f779ccbbbc986091e16c7dc3dc75e2cefc2f230
Amr Gamal <amr.aboulela97@gmail.com>
2024-06-08 00:43:18 +0300

    cc

--- Service-Hub/ServiceHub.DAL/Repository/ServiceRepository.cs
+++ Service-Hub/ServiceHub.DAL/Repository/ServiceRepository.cs
@@ -4,11 +4,6 @@ using ServiceHub.BL.Interface;
 using ServiceHub.DAL.DataBase;
 using ServiceHub.DAL.Entity;
 using ServiceHub.DAL.Helper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceHub.BL.Repository
 {
