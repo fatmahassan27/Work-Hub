aa9b1b01785b8905dd930fe0853bd27603fd2d94
Fatma Hassan <119891041+fatmahassan27@users.noreply.github.com>
2024-06-19 18:26:38 +0300

    Delete Service-Hub-Frontend directory

--- Service-Hub-Frontend/src/app/guards/login.guard.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-
-export const loginGuard: CanActivateFn = (route, state) => {
-  let router = inject(Router);
-  router.navigateByUrl("/login");
-  return false;
-};
